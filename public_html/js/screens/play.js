aba5cb5d5c4dacd12e663f03449a7da5160a3a49
Ki Hwan Chong <joelkh784@gmail.com>
2014-11-13 11:24:58 -0800

    no message

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -12,8 +12,10 @@ game.PlayScreen = me.ScreenObject.extend({
                 me.game.world.addChild(player, 3);
                 
                 me.input.bindKey(me.input.KEY.RIGHT, "right");
+                me.input.bindKey(me.input.KEY.LEFT, "left");
 
-		// add our HUD to the game world
+
+        // add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
 	},
